1afb2b11134e7a2a7518d61433f0bb81bd1e5f0c
noor fatima <kaspi0965@gmail.com>
2025-05-15 13:36:42 +0500

    cpp

---
+++ cpp/22-Diamond_Shape/main.cpp
@@ -0,0 +1,51 @@
+/*
+22: 
+✅ Diamond Shape: 
+                      *   
+                     ***  
+                    ***** 
+                   ******* 
+                    ***** 
+                     ***  
+                      *   
+
+ 🎯Hint:
+
+ 1):Combine upper and lower pyramid.
+ 2): First half: increasing stars.
+ 3): Second half: decreasing stars.
+
+*/
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    //upper
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int s = 4; s >= i; s--)
+        {
+            cout<<" ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    //lower
+    for (int i = 4; i >= 1; i--)
+    {
+        for (int s = 4; s >= i; s--)
+        {
+            cout<<" ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
