1fe3fee590023ff73766355657527a2f6ca817ba
noor fatima <kaspi0965@gmail.com>
2025-05-18 22:53:55 +0500

    cpp

--- cpp/41-Snake_Pattern/main.cpp
+++ cpp/41-Snake_Pattern/main.cpp
@@ -13,6 +13,7 @@
  1): Alternate direction for each row (use modulo % 2).
  2): Loop: Outer for rows, inner for columns.
  3): Counter: Use a single variable to increment.
+ 4): use a "temp" variable for storing the reverse order of "c".
 
 */
 #include<iostream>
@@ -20,40 +21,32 @@ using namespace std;
 
 int main()
 {
-   int c=1;
-        for (int i = 1; i <= 4; i++)
-        {
-             if ( i % 2 == 1 )
-             {
-                for (int j = 1; j <= 4; j++)
-                {
-                   cout<<c<<" ";
-                   c++;
-                }
-             }
-             else
-             {
-                 int temp = c + 3;  // since we’re going to print 4 numbers
-            for (int j = 1; j <= 4; j++)
-            {
-                cout << temp-- << " ";
-                c++;
-            }
-                
-             }
-             
-        cout<<endl;
-        
-        }
-    return 0;
+   int c = 1;   // c is the number counter, starts from 1
+
+   for (int i = 1; i <= 4; i++) // Outer loop for rows (1 to 4)
+   {
+       if (i % 2 == 1) // If row number is odd (1st, 3rd)
+       {
+           for (int j = 1; j <= 4; j++) // Print left to right
+           {
+               cout << c << " ";
+               c++; // Move to next number
+           }
+       }
+       else // If row number is even (2nd, 4th)
+       {
+           // We want to print 4 numbers in reverse
+           int temp = c + 3;  // Because we are about to print 4 numbers in reverse
+           for (int j = 1; j <= 4; j++)
+           {
+               cout << temp-- << " "; // Print and decrease
+               c++; // Still increase counter, because we used the value
+           }
+       }
+       cout << endl; // Move to next line after each row
+   }
+
+   return 0;
 }
 
-/*
 
-Objective of the Code:
-The code prints a 4×4 matrix with numbers from 1 to 16 in a zigzag row-wise fashion:
-
-Even-indexed rows (i % 2 == 0) are printed left to right
-Odd-indexed rows (i % 2 == 1) are printed right to left
-
-*/
